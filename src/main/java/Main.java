8f5cc404f0d7bab3db0ffa3d024ad18c7c30513a
Kevin Guggermaier <kevin.guggermaier@edu.fh-joanneum.at>
2019-03-25 10:56:06 +0100

    added Initial import

---
+++ src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("HELLO WORLD");
+
+        Calculator calc = new Calculator();
+//
+//        calc.add(20);	// currently highest number
+//        calc.add(4);	// currently lowester number
+//        calc.add(16);
+
+        // add other integers
+
+        // output lowest number
+
+        // output highest number
+
+        // output sum of all numbers
+    }
+}
